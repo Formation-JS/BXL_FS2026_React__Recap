35c50b05a5dec568bc4d5d23cb355146b87e8119
Aurélien St <aurelien.strimelle@bstorm.be>
2026-08-26 15:19:35 +0200

    Request Demo

--- src/components/Weather/Weather.jsx
+++ src/components/Weather/Weather.jsx
@@ -1,11 +1,25 @@
+import { Suspense, use } from 'react';
 
 export default function Weather({ coord }) {
 
-    // TODO Faire la requete :o
+    const apiKey = import.meta.env.VITE_WEATHER_KEY;
+
+    const promise = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coord.lat}&lon=${coord.lon}&units=metric&lang=fr&appid=${apiKey}`).then(r => r.json());
 
     return (
         <div>
-            Requete !!!
+            <Suspense fallback={<p>Ssssa charge...</p>}>
+                <WeatherInner promise={promise} />
+            </Suspense>
         </div>
+    )
+}
+
+function WeatherInner({ promise }) {
+    const data = use(promise);
+    const temp = data.main.temp;
+
+    return (
+        <p>La temperature est : {temp.toLocaleString('fr-be', { style: 'unit', unit: 'celsius'})}</p>
     )
 }
